07929539a3b29bd9dceac573dc4fc42b7326c278
Qaasim Umar <omayr010@gmail.com>
2023-09-07 21:09:12 +0100

    second commit

--- script.js
+++ script.js
@@ -1,26 +1,84 @@
-const cvv = document.getElementById("cvv");
-const cardNumber = document.getElementById("cardnumber");
+const cvv = document.querySelector("#cvv");
+// const numberInput = document.getElementsByClassName("small-box");
 const cardHolder = document.getElementById("cardholder");
 
 const form = document.getElementById("form");
 
 const err = document.getElementById("err");
+const nameErr = document.getElementById("name-err");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const numberInput = document.querySelector(".smallbox");
+  numberInput.addEventListener("input", function () {
+    const inputValue = numberInput.value;
+    const numericValue = inputValue.replace(/[^0-9]/g, "");
+    numberInput.value = numericValue;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const numberInput = document.querySelector("#cardnumber2");
+  numberInput.addEventListener("input", function () {
+    const inputValue = numberInput.value;
+    const numericValue = inputValue.replace(/[^0-9]/g, "");
+    numberInput.value = numericValue;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const numberInput = document.querySelector("#cardnumber3");
+  numberInput.addEventListener("input", function () {
+    const inputValue = numberInput.value;
+    const numericValue = inputValue.replace(/[^0-9]/g, "");
+    numberInput.value = numericValue;
+  });
+});
 
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const numberInput = document.querySelector("#cardnumber4");
+  numberInput.addEventListener("input", function () {
+    const inputValue = numberInput.value;
+    const numericValue = inputValue.replace(/[^0-9]/g, "");
+    numberInput.value = numericValue;
+  });
+});
+
+function moveToNext(currentInput, nextInputId) {
+  const inputValue = currentInput.value;
+  if (inputValue.length === 4) {
+    const nextInput = document.getElementById(nextInputId);
+    if (nextInput) {
+      nextInput.focus();
+    }
+  }
+}
 
 form.addEventListener("submit", (e) => {
-let messages = []
+  let cvvMessages = [];
+  let nameMessage = [];
 
-if(cvv.value.length < 4 || cvv.value.length > 4) {
+  if (cvv.value.length < 4 || cvv.value.length > 4) {
+    cvvMessages.push("cvv must be 4 digit ");
+  }
+  if (cvvMessages.length > 0) {
+    e.preventDefault();
+    err.innerText = cvvMessages.join(", ");
+  }
+  const value = cardHolder.value.trim(); // Remove leading/trailing spaces
+  const nameParts = value.split(" ");
+  if (nameParts.length !== 2) {
+    nameMessage.push("Enter both a firstName and a lastName");
+  }
+  if (nameMessage.length > 0) {
+    e.preventDefault();
+    nameErr.innerText = nameMessage.join(", ");
+  }
 
-    messages.push('cvv must be 4 digit ')
+  if ( numberInput !== " " && cardHolder !== " " && cvvHolder !== " "){
 
-}
-    if (messages.length > 0) {
-
-  e.preventDefault();
-err.innerText = messages.join(', ')
-    
+  alert('Your form has been submitted successfully');
 
 }
 });
